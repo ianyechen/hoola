1a90ccd6662d41a43aec1209bab4e15ffb67c99b
unknown <ianc99988@gmail.com>
2020-08-07 19:18:07 -0700

    Implemented graphics for cpu melding, added more melding logic for cpu

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class InGame(FloatLayout):
     # holds the 4 Player classes 
     players = []
     # holds the current player's widgets for displaying the cards
-    widgets = []
+    widgets = [[],[],[],[]]
     # holds the cards currently selected by the player in string ('h13')
     cards_currently_selected = []
     # holds which turn it is (0-3)
@@ -40,12 +40,26 @@ class InGame(FloatLayout):
         self.players = [Player() for i in range(4)]
       
         # initial distribution of cards at the beginning of game 
+
+        # count = 0
         for player in self.players:
             self.add_widget(player)
+
+            # if count == 0:
+            #     player.player_cards = [1,2,3,4,5,6,7]
+            #     count+=1
+            #     continue
+            # elif count == 1:
+            #     player.player_cards = [8,9,10,18,19,20]
+            #     count+=1
+            #     continue
+
             for i in range(7):
                 card = random.choice(self.card_deck)
                 self.card_deck.remove(card)
                 player.add_card(card)
+            
+            # count+=1
 
         self.add_widget(Touch())
         self.display_cards()
@@ -54,28 +68,14 @@ class InGame(FloatLayout):
 
     def display_cards(self):
 
-        increment = -(len(self.players[1].player_cards)//2) 
-
-        for card in self.players[1].player_cards:
-            self.add_widget(Image(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':increment*0.05, 'y': 0.9}))
-            increment += 1
-
-        increment = -(len(self.players[2].player_cards)//2) + 10
-
-        for card in self.players[2].player_cards:
-            self.add_widget(RotatedCards(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':-0.5, 'y': increment*0.05}))
-            increment += 1
-
-        increment = -(len(self.players[3].player_cards)//2) + 10
-
-        for card in self.players[3].player_cards:
-            self.add_widget(RotatedCards(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':0.5, 'y': increment*0.05}))
-            increment += 1
+        self.players[1].display_card(1)
+        self.players[2].display_card(2)
+        self.players[3].display_card(3)
 
         self.trash_pile_card = Image(source='./cards/back.png', size_hint_y=0.2, pos_hint={'x': 0.1, 'y':0.5})
         self.add_widget(self.trash_pile_card)
 
-        self.players[0].display_card()
+        self.players[0].display_card(0)
 
     def start_game(self):
         card = random.choice(self.card_deck)
@@ -86,10 +86,10 @@ class InGame(FloatLayout):
     
     def verify_meld(self):
         if is_meld_valid(self.cards_currently_selected):
-            print('Meld is succesful')
+            # print('Meld is succesful')
 
             increment = len(self.players[0].player_melded_cards)
-            print(self.players[0].player_melded_cards)
+            # print(self.players[0].player_melded_cards)
 
             for card in self.cards_currently_selected:             
                 if card == '': continue
@@ -99,7 +99,7 @@ class InGame(FloatLayout):
                 card_num = cardstr_to_cardnum(card)
                 self.players[0].remove_card(card_num)
                 self.players[0].player_melded_cards.append(card)
-                self.refresh_cards()
+                self.refresh_cards(0)
                 increment += 1
 
             self.cards_currently_selected.clear()
@@ -123,11 +123,11 @@ class InGame(FloatLayout):
         self.players[0].remove_card(card_num)
         self.trash_pile_card.source = cardnum_to_cardstr(card_num)
         self.trash_pile_card_num = card_num
-        self.refresh_cards()
+        self.refresh_cards(0)
         self.buttons_visible = False
         self.took_first_card = False
         self.turn += 1
-        self.players[self.turn+1].player_turn(self.turn+1)
+        self.players[self.turn].player_turn(self.turn)
 
     def draw_card(self, player):
         self.took_first_card = True
@@ -136,13 +136,13 @@ class InGame(FloatLayout):
         player.add_card(random_card)
         self.card_deck.remove(random_card)
         self.number_of_cards_left = len(self.card_deck)
-        self.refresh_cards()
+        self.refresh_cards(0)
 
-    def refresh_cards(self):
-        for widget in self.widgets:
+    def refresh_cards(self, player_number):
+        for widget in self.widgets[player_number]:
             self.remove_widget(widget)
         
-        self.players[0].display_card()
+        self.players[player_number].display_card(player_number)
 
     def check_for_thank_yous(self):
         Clock.schedule_interval(self.set_deciding_thank_you, 0.1)
@@ -156,7 +156,7 @@ class InGame(FloatLayout):
         self.turn = 0
         self.cards_currently_selected.clear()
         self.players[0].add_card(self.trash_pile_card_num)
-        self.refresh_cards()
+        self.refresh_cards(0)
         self.trash_pile_card.source = './cards/back.png'
         self.trash_pile_card_num = None
         self.deciding_thank_you = False
@@ -168,14 +168,13 @@ class InGame(FloatLayout):
 
         if self.turn == 4:
             self.turn = 0
-            print(self.turn)
+            # print(self.turn)
             self.buttons_visible = True
             self.took_first_card = False
             return
 
         self.players[self.turn].player_turn(self.turn)
 
-    
 class HoolaApp(App):
     def build(self):
         return InGame()
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 
 from funcs import cardnum_to_cardstr
+from rotated_cards import RotatedCards
 
 class Player(Widget):
 
@@ -13,6 +14,7 @@ class Player(Widget):
     def __init__(self, **kwargs):
         super(Player, self).__init__(**kwargs)
         self.player_cards = []
+        self.player_melded_cards = []
 
     def add_card(self, card):
         self.player_cards.append(card)
@@ -20,16 +22,39 @@ class Player(Widget):
     def remove_card(self, card):
         self.player_cards.remove(card)
 
-    def display_card(self):
-
+    def display_card(self, player_number):
+        
         increment = -(len(self.player_cards)//2) 
+        if player_number == 2 or player_number == 3: increment += 10
 
-        for card in self.player_cards:
-            img_src = cardnum_to_cardstr(card)
-            wid = Image(source=img_src, size_hint_y=0.2, pos_hint={'x':increment*0.05, 'y': 0})
-            self.parent.add_widget(wid)
-            self.parent.widgets.append(wid)
-            increment += 1        
+        if player_number == 0:
+            for card in self.player_cards:
+                img_src = cardnum_to_cardstr(card)
+                wid = Image(source=img_src, size_hint_y=0.2, pos_hint={'x':increment*0.05, 'y': 0})
+                self.parent.add_widget(wid)
+                self.parent.widgets[0].append(wid)
+                increment += 1     
+
+        elif player_number == 1:
+            for card in self.player_cards:
+                wid = Image(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':increment*0.05, 'y': 0.95})
+                self.parent.add_widget(wid)
+                self.parent.widgets[1].append(wid)
+                increment += 1
+
+        elif player_number == 2:
+            for card in self.player_cards:
+                wid = RotatedCards(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':-0.5, 'y': increment*0.05})
+                self.parent.add_widget(wid)
+                self.parent.widgets[2].append(wid)
+                increment += 1
+
+        else:
+            for card in self.player_cards:
+                wid = RotatedCards(source='./cards/back.png', size_hint_y=0.1, pos_hint={'x':0.5, 'y': increment*0.05})
+                self.parent.add_widget(wid)
+                self.parent.widgets[3].append(wid)
+                increment += 1
 
     def player_turn(self, player_number):
         
@@ -40,6 +65,7 @@ class Player(Widget):
             self.decide_can_meld(player_number)
         # print(self.player_cards)
         self.parent.end_turn(player_number)
+        self.parent.refresh_cards(player_number)
         # print(self.player_cards)
         # return player_number + 1 if (player_number + 1 != 4) else 0 
 
@@ -47,11 +73,13 @@ class Player(Widget):
 
         print('Melding for player, ', player_number)
         self.player_cards.sort()
+
+        # key is the number of card (1-13), value is the index where the number is at
         num_to_index = {}
         combinations_that_can_be_melded = []
         copy_of_player_cards = list(self.player_cards)
 
-        print('Player cards ->', self.player_cards)
+        # converting the entire list of player cards into the numbers by % 13 (27 -> 1)
         for i in range(len(copy_of_player_cards)):
             copy_of_player_cards[i] %= 13
             if copy_of_player_cards[i] == 0: copy_of_player_cards[i] = 13
@@ -59,18 +87,71 @@ class Player(Widget):
                 num_to_index[copy_of_player_cards[i]] = []
             num_to_index[copy_of_player_cards[i]].append(i)
 
-        print('Player cards ->', self.player_cards)
-        print('Player cards copy ->', copy_of_player_cards)
-        print(num_to_index)
+        print('Player cards ->', copy_of_player_cards)
 
+        same_suit_cards = []
+
+        for i in range(len(copy_of_player_cards)):
+
+            same_suit_cards.append(copy_of_player_cards[i])
+
+            if i + 1 < len(copy_of_player_cards) and copy_of_player_cards[i+1] < copy_of_player_cards[i]:
+                
+                print('same suit cards -> ', same_suit_cards)
+                if len(same_suit_cards) == 3:
+
+                    # for j in range(len(same_suit_cards)):
+
+                    if ((same_suit_cards[2] == same_suit_cards[1] + 1 == same_suit_cards[0] + 2) 
+                    or (12 in same_suit_cards and 13 in same_suit_cards and 1 in same_suit_cards)
+                    or (13 in same_suit_cards and 1 in same_suit_cards and 2 in same_suit_cards)):
+                        self.player_melded_cards.append(same_suit_cards)
+
+                same_suit_cards = []
+
+        # getting all the 7's 
         if num_to_index.get(7) != None:
-            combinations_that_can_be_melded.append(num_to_index[7])
+            for i in range(len(num_to_index[7])):
+                num_to_index[7][i] = self.player_cards[num_to_index[7][i]]
 
+            self.player_melded_cards.append(num_to_index[7])
+
+        # getting the combinations of 3 of the same card 
         for key, value in num_to_index.items():
             if len(value) >= 3:
-                combinations_that_can_be_melded.append(value)
+                for i in range(len(value)):
+                    value[i] = self.player_cards[value[i]]
 
+                self.player_melded_cards.append(value)
 
-        print(combinations_that_can_be_melded)
+        if self.player_melded_cards: self.display_melded_cards(player_number)
+        print('Melded cards -> ', self.player_melded_cards)
+        print('Player cards -> ', self.player_cards)
 
+    def display_melded_cards(self, player_number):
 
+        increment_x = 0
+        increment_y = 0
+        pos_hint = {}
+
+        for combination in self.player_melded_cards:        
+
+            for card in combination:
+
+                img_src = cardnum_to_cardstr(card)     
+
+                if player_number == 1:
+                    pos_hint = {'x':-0.2 + increment_x*0.05, 'y': 0.75}
+
+                elif player_number == 2: 
+                    pos_hint = {'x':-0.4 + increment_x*0.05, 'y': 0.5 - increment_y*0.15}
+        
+                elif player_number == 3:
+                    pos_hint = {'x':0.3 + increment_x*0.05, 'y': 0.5 - increment_y*0.15}
+
+                self.parent.add_widget(Image(source=img_src, size_hint_y=0.15, pos_hint=pos_hint))
+                increment_x += 1
+                if card in self.player_cards: self.player_cards.remove(card)
+
+            if player_number > 1: increment_x = 0
+            increment_y += 1
